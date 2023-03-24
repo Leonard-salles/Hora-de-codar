5b5cf15919374f1e891060e5cf3f594c80d9a16e
leonard-salles <lsleonardosalles.999@gmail.com>
2023-03-24 09:15:46 -0300

    atualização

---
+++ 1_INTRO/hello-world/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h2>Emmet funcionou</h2>
+      <p>é só um teste</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -0,0 +1,15 @@
+// components
+import FirstComponent from './Components/FirstComponent';
+
+// styles
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
